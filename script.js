8e148d4848886493433078af897406253a0a0140
jmauro33 <jonathanmauro16@yahoo.com>
2019-12-17 18:08:39 -0600

    Add new files

---
+++ script.js
@@ -0,0 +1,28 @@
+
+
+
+var generatebtn=document.querySelector("#generate");
+
+function generatePassword (){
+   
+      
+}
+
+
+function writePassword (){
+
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
+
+    passwordText.value = password;
+
+    copyBtn.removeAttribute("#disabled");
+    copyBtn.focus();
+
+}
+function copyToClipboard(){
+
+
+
+
+}
