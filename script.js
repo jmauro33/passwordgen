6d19d567d1ddf4a7fb276199721ed4498f1f1336
jmauro33 <jonathanmauro16@yahoo.com>
2019-12-21 14:56:57 -0600

    got password to work

--- script.js
+++ script.js
@@ -1,40 +1,50 @@
 
-var generatebtn=document.querySelector("#generate");
-function generatePassword(length){
+var generateBtn = document.querySelector("#generate");
+var copyBtn = document.querySelector("#copy");
 
+function generatePassword(min) {
+    var alpha = "abcdefghijklmnopqrstuvwxyz";
+    var caps = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    var numeric = "0123456789";
+    var special = "!@#$%^&*()";
 
-var alpha = "abcdefghijklmnopqrstuvwxyz";
-var caps = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var numeric = "0123456789";
-var special = "!@#$%^&*()";
+    var charPool = alpha.concat(caps, numeric, special);
 
-var options = [alpha, caps, numeric, special];
+    var password = "";
+    var passwordArray = Array(min);
+    
+    // loop as times as defined by min variable
 
-var password = "";
-var passwordArray = Array(length); 
-var passwordRandom = math.floor(math.random() * (max - min + 1)) +min;
-return password;
+    
+    for(i=0; i < min; i++) {
+        var idx = Math.floor(Math.random() * charPool.length);
+        password = password + charPool[idx]
+       
+    }
+   
+    return password;
 }
 
-console.log(generatePassword)
+generatePassword(8);
 
 
-
-
-
-
-function writePassword (){
-
-    var password = generatePassword();
+function writePassword() {
+    var password = generatePassword(8);
     var passwordText = document.querySelector("#password");
 
     passwordText.value = password;
 
-    console.log(passwordText)
-
     copyBtn.removeAttribute("#disabled");
     copyBtn.focus();
-
 }
-function copyToClipboard(){}
 
+function copyToClipboard() {
+
+    var copyText = document.getElementById("#password");
+    copyText.select();
+    document.execCommand("copy");
+    
+}
+
+generateBtn.addEventListener("click", writePassword);
+copyBtn.addEventListener("click", copyToClipboard);
